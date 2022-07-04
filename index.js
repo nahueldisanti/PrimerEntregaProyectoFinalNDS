ae1bc9d9d1fdac19f0c08c3f91d4e655f5397ac2
nahuel di santi <nahuel.disanti@gmail.com>
2022-07-04 20:44:41 -0300

    version sin la posibilidad de agregar provincia

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
 let nombre = prompt("Bienvenide a Punto de Observacion, tu calculadora de viajes. Cual es tu nombre?");
 
 
-//Constructor de Provincias INICIO
+//Constructor de Provincias INICIO (todavia no lo pude terminar este)
 
-// class Provincia{
-//     constructor(nombre, distancia, valorHotel, datoProvincia) {
-//     this.nombre= nombre;
-//     this.distancia=distancia;
-//     this.valorHotel= valorHotel;
-//     this.datoProvincia= () =>{console.log(datoProvincia)};
-//     this.disponibilidad= 5;
-//     }
-
-//     ocupar() {
-//         this.disponibilidad -= 1;
-//         if (this.disponibilidad === 0){
-//             alert("Ups, ya no tenemos lugar para esa provincia");
-//         }
-//     }
-// }
+class Provincia{
+    constructor(nombre, distancia, valorHotel, datoProvincia) {
+    this.nombre= nombre;
+    this.distancia=distancia;
+    this.valorHotel= valorHotel;
+    this.datoProvincia= () =>{console.log(datoProvincia)};
+    this.disponibilidad= 5;
+    }
+    ocupar() {
+        this.disponibilidad -= 1;
+        if (this.disponibilidad === 0){
+            alert("Ups, ya no tenemos lugar para esa provincia");
+        }
+    }
+}
 
 //Array de provincias disponibles 
 provincias= [
-    { nombre: "Cordoba", distancia: 800 , valorHotel: 1500 , datoProvincia: "En Cordoba podes visitar Tras La Sierra, zona de montes y rios ideal para disfrutar en verano." },
-    { nombre: "Santa Fe", distancia: 500 , valorHotel: 1000 , datoProvincia: "En Santa Fe capital, se encuentra el rio Parana. Tomate un Amargo Obrero en la ribera." },
-    { nombre: "Chubut", distancia: 900 , valorHotel: 2500 , datoProvincia: "Si visitas la provincia de Chubut entre los meses de septiembre a diciembre, podras ver ballenas francas australes nadando por las costas de Puerto Madryn."  },
-    { nombre: "Rio Negro", distancia: 1000 , valorHotel:3000 , datoProvincia: "Rio Negro, una de las provincias con mas atractivos turisticos de la Patagonica. Algunos destinos imperdibles son: Bariloche, San Martin de los Andes, Villa La Angostura y muchos mas. " },
+    { nombre: "cordoba", distancia: 800 , valorHotel: 1500 , datoProvincia: "En Cordoba podes visitar Tras La Sierra, zona de montes y rios ideal para disfrutar en verano." },
+    { nombre: "santa fe", distancia: 500 , valorHotel: 1000 , datoProvincia: "En Santa Fe capital, se encuentra el rio Parana. Tomate un Amargo Obrero en la ribera." },
+    { nombre: "chubut", distancia: 900 , valorHotel: 2500 , datoProvincia: "Si visitas la provincia de Chubut entre los meses de septiembre a diciembre, podras ver ballenas francas australes nadando por las costas de Puerto Madryn."  },
+    { nombre: "rio negro", distancia: 1000 , valorHotel:3000 , datoProvincia: "Rio Negro, una de las provincias con mas atractivos turisticos de la Patagonica. Algunos destinos imperdibles son: Bariloche, San Martin de los Andes, Villa La Angostura y muchos mas. " },
 ]
 
 //Funciones necesarias para la calculadora
@@ -62,27 +61,20 @@ function verDato (provincia){
         console.log(provincia['datoProvincia']);
     }
     else {
-        alert("Que tengas buen viaje!")
+        alert("Que tengas buen viaje!");
     }
 }
 
-function opcionesDestino() {
-    let opciones = prompt("Un gusto saludarte, "+nombre+". Por favor, ingresa a que provincia te gustaria ir. Si queres salir, ingresa ESC");
-    console.log(opciones)
-    return opciones;
+function buscadorDestino() {
+    const destino = prompt("Un gusto saludarte, "+ nombre +". Por favor, ingresa a que provincia te gustaria ir. Si queres salir, ingresa ESC(Opciones disponibles: Cordoba, Rio Negro, Chubut, Santa Fe");
+    console.log(destino);
+    const  provinciaEncontrada = provincias.find( provincia => provincia.nombre === destino.toLowerCase() ); //Me gustaria lograr aca usar el toLowerCase, pero me hace fallar el codigo y no se que puede ser.
+    console.log(provinciaEncontrada);
+    return provinciaEncontrada;
 }
-
-function buscadorDestino(destino) {
-    const  provinciaEncontrada = provincias.find( (provincia) => provincia.nombre.toLowerCase === destino.toLowerCase );
-    console.log(provinciaEncontrada)
-    return provinciaEncontrada
-}
-
-//Funcion Calculadora
-
 
 function calculadora() {
-    let destinoSeleccionado=buscadorDestino(opcionesDestino());
+    let destinoSeleccionado=buscadorDestino();
     console.log(destinoSeleccionado['valorHotel']);
     console.log(destinoSeleccionado['distancia']);
     while (destinoSeleccionado !== "ESC") {
